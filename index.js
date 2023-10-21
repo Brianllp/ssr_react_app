5ef42e2202afa3091356da7bf34fe876b34453c0
Brianllp <44490577+Brianllp@users.noreply.github.com>
2023-10-22 02:06:38 +0900

    add: webpack & babel

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import ssr from "./views/ssr.js";
 
 const app = express();
 const port = 9000;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.use(express.static('assets'));
+
+app.get("/ssr_sample", (req, res) => {
+  const response = ssr();
+  res.send(response);
 });
 
 app.listen(port, () => {
